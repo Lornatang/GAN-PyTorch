30dcb5fbc32bb273d3433f0523c6e9964ae52503
Changyu <liuchangyu1111@gmail.com>
2020-12-18 15:05:00 +0800

    update API version

--- test.py
+++ test.py
@@ -53,7 +53,7 @@ if __name__ == "__main__":
 
     logger.info("TestEngine:")
     print("\tAPI version .......... 0.1.0")
-    print("\tBuild ................ 2020.11.30-1116-0c5adc7e")
+    print("\tBuild ................ 2020.12.18-1454-f636e462")
 
     logger.info("Creating Testing Engine")
     device = select_device(args.device)
