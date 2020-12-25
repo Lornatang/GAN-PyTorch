ad5375fae8811a4425f6ad467e2a8321f5faf603
Changyu <liuchangyu1111@gmail.com>
2020-12-25 16:50:30 +0800

    Ensure that the discriminator architecture is error free

--- train.py
+++ train.py
@@ -70,10 +70,10 @@ if __name__ == "__main__":
                         help="The height / width of the input image to network. (default: 28).")
     parser.add_argument("--channels", type=int, default=1,
                         help="The number of channels of the image. (default: 1).")
-    parser.add_argument("--hidden-channels", type=int, default=1200,
-                        help="The number of hidden channels of the image. (default: 1200).")
-    parser.add_argument("--lr", type=float, default=3e-4,
-                        help="Learning rate. (default:3e-4)")
+    parser.add_argument("--hidden-channels", type=int, default=240,
+                        help="The number of hidden channels of the discriminator. (default: 240).")
+    parser.add_argument("--lr", type=float, default=0.0002,
+                        help="Learning rate. (default:0.0002)")
     args = parser.parse_args()
 
     print("##################################################\n")
